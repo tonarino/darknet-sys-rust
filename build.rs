918257a1272bd0609abab1bf4073f3e2d0dbb79e
Brian Schwind <brianmschwind@gmail.com>
2022-07-21 15:39:10 +0900

    Rebuild on darknet changes, update darknet revision

--- build.rs
+++ build.rs
@@ -262,6 +262,7 @@ fn build_from_source() -> Result<()> {
 }
 
 fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=darknet/");
     println!("cargo:rerun-if-env-changed={}", DARKNET_SRC_ENV);
     println!("cargo:rerun-if-env-changed={}", DARKNET_INCLUDE_PATH_ENV);
     println!("cargo:rerun-if-env-changed={}", CUDA_PATH_ENV);
